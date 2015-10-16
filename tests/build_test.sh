eeb873e35f122454109672089d19b5478f86853e
Braxton Huggins <braxton@pagodabox.com>
2015-10-16 16:24:05 -0600

    try to fix the tests

--- tests/build_test.sh
+++ tests/build_test.sh
@@ -5,4 +5,8 @@ pass "unable to start the $VERSION container" docker run --privileged=true -d --
 
 defer docker kill $UUID
 
+pass "Failed to create /opt/code directory" docker exec $UUID mkdir -p /opt/code
+
+pass "Failed to create /code directory" docker exec $UUID mkdir -p /code
+
 pass "Failed to run build script" docker exec $UUID bash -c "cd /opt/engines/php/bin; ./build '$(payload default-build)'"
--- tests/prepare_test.sh
+++ tests/prepare_test.sh
@@ -5,4 +5,8 @@ pass "unable to start the $VERSION container" docker run --privileged=true -d --
 
 defer docker kill $UUID
 
+pass "create db dir for pkgsrc" docker exec $UUID mkdir -p /data/var/db
+
+pass "Failed to update pkgsrc" docker exec $UUID /data/bin/pkgin up -y
+
 pass "Failed to run prepare script" docker exec $UUID bash -c "cd /opt/engines/php/bin; ./prepare '$(payload default-prepare)'"
