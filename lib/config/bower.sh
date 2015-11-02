6b873ab551f129eb90b9e05ec85c0137a5ea640d
Braxton Huggins <braxton@pagodabox.com>
2015-11-02 15:16:11 -0700

    replace hard tabs with spaces

--- lib/config/bower.sh
+++ lib/config/bower.sh
@@ -3,22 +3,22 @@
 
 has_bower() {
   if [[ -f $(code_dir)/bower.json ]]; then
-  	echo "true"
+    echo "true"
   else
-  	echo "false"
+    echo "false"
   fi
 }
 
 install_bower() {
   if [[ "$(has_bower)" = "true" ]]; then
-  	print_bullet_info "Found bower.json, installing bower"
+    print_bullet_info "Found bower.json, installing bower"
     (cd $(code_dir); run_process "npm install bower" "npm install bower")
   fi
 }
 
 bower_install() {
   if [[ "$(has_bower)" = "true" ]]; then
-  	print_bullet_info "Found bower.json, running 'bower install'"
-  	(cd $(code_dir); run_process "bower install" "node_modules/.bin/bower --config.interactive=false install")
+    print_bullet_info "Found bower.json, running 'bower install'"
+    (cd $(code_dir); run_process "bower install" "node_modules/.bin/bower --config.interactive=false install")
   fi
 }
