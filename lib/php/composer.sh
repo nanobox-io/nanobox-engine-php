77a9a213e120e49966e6689af1efb2b81a9c3658
Braxton Huggins <braxton@pagodabox.com>
2017-12-14 14:48:22 -0700

    add support for prestissimo

--- lib/php/composer.sh
+++ lib/php/composer.sh
@@ -12,11 +12,20 @@ composer_required_extensions() {
   echo "${exts[@]}"
 }
 
+composer_use_prestissimo() {
+  # boxfile composer_use_prestissimo
+  composer_use_prestissimo=$(nos_validate "$(nos_payload config_composer_use_prestissimo)" "boolean" "false")
+  echo "$composer_use_prestissimo"
+}
+
 # Runs composer install
 composer_install() {
   if [[ -f $(nos_code_dir)/composer.json ]]; then
     if [[ ! -f $(nos_code_dir)/composer.lock ]]; then
       nos_print_warning "No 'composer.lock' file detected. This may cause a slow or failed build. To avoid this issue, commit the 'composer.lock' file to your git repo."
+    fi
+    if [[ "$(composer_use_prestissimo)" = "true" ]]; then
+      (cd $(nos_code_dir); nos_run_process "composer global require hirak/prestissimo" "composer global require hirak/prestissimo")
     fi
     (cd $(nos_code_dir); nos_run_process "composer install" "composer install --no-interaction --prefer-source")
   fi
--- test/tests/integration/composer.bats
+++ test/tests/integration/composer.bats
@@ -12,7 +12,7 @@ payload() {
   "cache_dir": "/tmp/cache",
   "etc_dir": "/data/etc",
   "env_dir": "/data/etc/env.d",
-  "config": {}
+  "config": {"composer_use_prestissimo": "true"}
 }
 END
 }
